64562d0c9b09bc1e1bcc4c6ed578b975440d1541
Son <tungson.ktct@gmail.com>
2022-12-21 23:01:34 +0700

    init project1

---
+++ reactjdon/src/App.js
@@ -0,0 +1,19 @@
+import Class from "./Component/Class";
+import Footer from "./Component/Footer";
+import Library from "./Component/Library";
+import Slider from "./Component/Slider";
+import Student from "./Component/Student";
+
+const App = () => {
+  return (
+    <>
+      <Slider />
+      <Student />
+      <Class />
+      <Library />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
